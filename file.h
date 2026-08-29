2083b6923ae6782477b7a7bf8b0305b63d2c41fd
Karthik-Jupalli <jupallikarthik04@gmail.com>
2026-08-29 14:39:25 +0530

    Remove accidentally uploaded files

--- file.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-#include "contact.h"
-//prototypes of functions for file
-void saveContactsToFile(AddressBook *addressBook);
-void loadContactsFromFile(AddressBook *addressBook);
-
-#endif
